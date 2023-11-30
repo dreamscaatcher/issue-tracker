0fe0817fd9715092f7f229e200cc231517792f23
Gurinder Singh Ghuman <dreamscaatcher@gmail.com>
2023-11-30 07:22:45 +0100

    Refactor Move inline function

--- issues-tracker/app/issues/new/page.tsx
+++ issues-tracker/app/issues/new/page.tsx
@@ -26,6 +26,15 @@ const NewIssuePage = () => {
 	const [error, setError] = useState("");
 	const[isSubmitting, setSubmitting] =useState(false);
 
+	const onSubmit = handleSubmit(async (data) => {
+		try {setSubmitting(true)
+			await axios.post("/api/issue", data);
+			router.push("/issues");
+		} catch (error) {setSubmitting(false)
+			setError("An unexpected error occured. ");
+		}
+	})
+
 	return (
 		<div className="max-w-xl">
 			{error && (
@@ -38,14 +47,7 @@ const NewIssuePage = () => {
 			)}
 			<form
 				className=" space-y-4"
-				onSubmit={handleSubmit(async (data) => {
-					try {setSubmitting(true)
-						await axios.post("/api/issue", data);
-						router.push("/issues");
-					} catch (error) {setSubmitting(false)
-						setError("An unexpected error occured. ");
-					}
-				})}
+				onSubmit={onSubmit}
 			>
 				<TextField.Root>
 					<TextField.Input
